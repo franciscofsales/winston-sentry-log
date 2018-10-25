470eb1a77f4201e72ffcc084e05a6cbcbdd8887e
Francisco Sales <franciscofsales1@gmail.com>
2018-10-25 10:13:16 +0100

    remove node_modules

--- node_modules/@sentry/utils/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// We do not export anything by default since we always want to deep import
-// like: import { getGlobalObject } from '@sentry/utils/misc';
-//# sourceMappingURL=index.js.map
